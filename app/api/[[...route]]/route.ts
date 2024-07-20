c0a13d1794d47578e2263af0106c3c7c0535b655
Marty <marty@kuori.cz>
2024-07-20 19:18:04 +0200

    feat: Refactor API routes to use separate file for accounts

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -6,23 +6,15 @@ import { zValidator } from "@hono/zod-validator";
 
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
+import accounts from "./accounts";
+
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      error: "Unauthorized",
-    });
-  }
-
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
