d40e3841cb07f87b2f294666cb97fade327a0c39
Marty <marty@kuori.cz>
2024-07-20 13:59:50 +0200

    feat: Add mobile navigation menu with lazy loading for images

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useMedia } from "react-use";
 
 import { NavButton } from "@/components/nav-button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 const routes = [
   {
@@ -30,7 +35,47 @@ const routes = [
 type Props = {};
 
 export const Navigation = ({}: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const pathname = usePathname();
+  const router = useRouter();
+
+  const isMobile = useMedia("(max-width: 1024px)", false);
+
+  const onClick = (href: string) => {
+    router.push(href);
+    setIsOpen(false);
+  };
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            className="font-normal bg-white/10 text-white hover:bg-white/20 focus-visible:ring-offset-0 focus-visible:ring-transparent"
+          >
+            <Menu className="size-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side={"left"} className="px-2">
+          <nav className="flex flex-col gap-y-2 pt-6">
+            {routes.map((route) => (
+              <Button
+                variant={route.href === pathname ? "secondary" : "ghost"}
+                key={route.href}
+                onClick={() => onClick(route.href)}
+                className="w-full justify-start text-black"
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
+  }
 
   return (
     <nav className="hidded lg:flex items-center gap-x-2 overflow-x-auto">
