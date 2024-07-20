d69f82f3a0639fef11ca2a72416545e7fbfe6c50
Marty <marty@kuori.cz>
2024-07-20 21:52:41 +0200

    chore: Refactor accounts API route and add authentication middleware

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,34 +1,65 @@
 import { db } from "@/db/drizzle";
-import { accounts } from "@/drizzle/schema/schema";
+import { accounts, insertAccountSchema } from "@/drizzle/schema/schema";
 
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
-import { HTTPException } from "hono/http-exception";
+import { zValidator } from "@hono/zod-validator";
+
 import { Hono } from "hono";
 import { eq } from "drizzle-orm";
 
-const app = new Hono().get("/", clerkMiddleware(), async (c) => {
-  const auth = getAuth(c);
+import { createId } from "@paralleldrive/cuid2";
 
-  if (!auth) {
-    throw new HTTPException(401, {
-      res: c.json({ error: "Unauthorized" }, 401),
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+
+    //#region  //! sample for older versions of HONO
+    // if (!auth) {
+    //   throw new HTTPException(401, {
+    //     res: c.json({ error: "Unauthorized" }, 401),
+    //   });
+    // }
+    // if (!auth.userId) {
+    //   throw new HTTPException(401, {
+    //     res: c.json({ error: "UserId not valid " }, 401),
+    //   });
+    // }
+    //#endregion
+
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    const data = await db
+      .select({ id: accounts.id, name: accounts.name })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
+
+    return c.json({
+      data,
     });
-  }
-  if (!auth.userId) {
-    throw new HTTPException(401, {
-      res: c.json({ error: "UserId not valid " }, 401),
-    });
-  }
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator("json", insertAccountSchema.pick({ name: true })),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
 
-  const data = await db
-    .select({ id: accounts.id, name: accounts.name })
-    .from(accounts)
-    .where(eq(accounts.userId, auth.userId));
+      if (!auth?.userId) {
+        return c.json({ error: "UserId not valid " }, 401);
+      }
 
-  return c.json({
-    data,
-  });
-});
+      const data = await db.insert(accounts).values({
+        id: createId(),
+        userId: auth.userId,
+        ...values,
+      });
+
+      return c.json({});
+    },
+  );
 
 export default app;
