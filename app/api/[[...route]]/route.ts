3ac84a4bf1fcd68d4dcf22169029404e6df8ec1f
Marty <marty@kuori.cz>
2024-07-20 12:52:42 +0200

    refactor: Refactor API route for authenticated hello message

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -5,30 +5,24 @@ import z from "zod";
 import { zValidator } from "@hono/zod-validator";
 
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { error } from "console";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get(
-  "/hello",
+app.get("/hello", clerkMiddleware(), (c) => {
+  const auth = getAuth(c);
 
-  clerkMiddleware(),
-  (c) => {
-    const auth = getAuth(c);
-
-    if (!auth?.userId) {
-      return c.json({
-        error: "Unauthorized",
-      });
-    }
-
+  if (!auth?.userId) {
     return c.json({
-      message: "Hello Next.js!",
+      error: "Unauthorized",
     });
-  },
-);
+  }
+
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
