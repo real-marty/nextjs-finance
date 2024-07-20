f8f4dc09272cea9876a06c152a48cf55ec8fc1b8
Marty <marty@kuori.cz>
2024-07-20 12:11:28 +0200

    feat: Add sign-up page component with Clerk integration

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,29 @@
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+import { SignUp, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-muted-foreground">
+            Welcome back!
+          </h1>
+          <p>Log-in or Create accont to get back into your dashboard!</p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin anima text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image src={"/logo.svg"} height={100} width={100} alt="logo" />
+      </div>
+    </div>
+  );
+}
