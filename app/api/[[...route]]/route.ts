63833c5501a16dea636a5acea0066952ca04b3ca
Marty <marty@kuori.cz>
2024-07-20 20:00:44 +0200

    chore: Handle HTTP exceptions in API route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,16 +1,19 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
-import z from "zod";
-import { zValidator } from "@hono/zod-validator";
-
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-
 import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+  return c.json({ error: "Internal Server Error" }, 500);
+});
 
 const routes = app.route("/accounts", accounts);
 
