fc53eeadcef40ff7f5c754be511f8b993c030eed
Marty <marty@kuori.cz>
2024-07-20 20:33:30 +0200

    feat: Add authentication middleware to accounts API route

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -5,6 +5,7 @@ import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 import { HTTPException } from "hono/http-exception";
 import { Hono } from "hono";
+import { eq } from "drizzle-orm";
 
 const app = new Hono().get("/", clerkMiddleware(), async (c) => {
   const auth = getAuth(c);
@@ -14,10 +15,16 @@ const app = new Hono().get("/", clerkMiddleware(), async (c) => {
       res: c.json({ error: "Unauthorized" }, 401),
     });
   }
+  if (!auth.userId) {
+    throw new HTTPException(401, {
+      res: c.json({ error: "UserId not valid " }, 401),
+    });
+  }
 
   const data = await db
     .select({ id: accounts.id, name: accounts.name })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
 
   return c.json({
     data,
