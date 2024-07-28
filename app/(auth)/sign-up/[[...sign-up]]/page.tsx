af6da1a9476a3bcb75c84b8b1ebab98c74b808e4
Marty <marty@kuori.cz>
2024-07-28 17:09:05 +0200

    chore: Update logo source path in sign-up page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
         </div>
       </div>
       <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
-        <Image src={"/logo.svg"} height={100} width={100} alt="logo" />
+        <Image src={"/branding/logo.svg"} height={100} width={100} alt="logo" />
       </div>
     </div>
   );
