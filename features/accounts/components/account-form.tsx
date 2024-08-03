5f5240865069eaac29981795413f9f82e58735c0
Marty <marty@kuori.cz>
2024-08-03 11:10:29 +0200

    refactor: Create AccountForm component for managing account creation and deletion

---
+++ features/accounts/components/account-form.tsx
@@ -0,0 +1,92 @@
+import { z } from "zod";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { insertAccountSchema } from "@/db/drizzle/schema/schema";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
+
+type Props = {
+  id?: string;
+  defaultValues?: FormValues;
+  onSublit: (values: FormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+export const AccountForm = ({
+  id,
+  defaultValues,
+  onSublit,
+  onDelete,
+  disabled,
+}: Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    onSublit(values);
+  };
+
+  const handleDelete = () => {
+    onDelete?.();
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-4 pt-4"
+      >
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor={field.name}>Name</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder="e.g. Cash, Back, Credit Card"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className="w-full">
+          {id ? "Save changes" : "Create account"}
+        </Button>
+        {!!id && (
+          <Button
+            variant={"outline"}
+            className="w-full"
+            type="button"
+            disabled={disabled}
+            onClick={handleDelete}
+          >
+            <Trash className="size-4 mr-2" />
+            {"  "}Delete account
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
