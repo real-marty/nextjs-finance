63a37e22fabc6b3cdfbf6543fff2d1152318a174
Marty <marty@kuori.cz>
2024-08-04 14:20:06 +0200

    refactor: Update accounts API to include bulk delete functionality

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -6,40 +6,43 @@ import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
 
 import { Hono } from "hono";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 
 import { createId } from "@paralleldrive/cuid2";
+import { z } from "zod";
 
 const app = new Hono()
-  .get("/", clerkMiddleware(), async (c) => {
-    const auth = getAuth(c);
+  .get(
+    "/", clerkMiddleware(), async (c) => {
+      const auth = getAuth(c);
 
-    //#region  //! sample for older versions of HONO
-    // if (!auth) {
-    //   throw new HTTPException(401, {
-    //     res: c.json({ error: "Unauthorized" }, 401),
-    //   });
-    // }
-    // if (!auth.userId) {
-    //   throw new HTTPException(401, {
-    //     res: c.json({ error: "UserId not valid " }, 401),
-    //   });
-    // }
-    //#endregion
+      //#region  //! sample for older versions of HONO
+      // if (!auth) {
+      //   throw new HTTPException(401, {
+      //     res: c.json({ error: "Unauthorized" }, 401),
+      //   });
+      // }
+      // if (!auth.userId) {
+      //   throw new HTTPException(401, {
+      //     res: c.json({ error: "UserId not valid " }, 401),
+      //   });
+      // }
+      //#endregion
 
-    if (!auth?.userId) {
-      return c.json({ error: "Unauthorized" }, 401);
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const data = await db
+        .select({ id: accounts.id, name: accounts.name })
+        .from(accounts)
+        .where(eq(accounts.userId, auth.userId));
+
+      return c.json({
+        data,
+      });
     }
-
-    const data = await db
-      .select({ id: accounts.id, name: accounts.name })
-      .from(accounts)
-      .where(eq(accounts.userId, auth.userId));
-
-    return c.json({
-      data,
-    });
-  })
+  )
   .post(
     "/",
     clerkMiddleware(),
@@ -63,6 +66,31 @@ const app = new Hono()
 
       return c.json({ data });
     },
-  );
+  )
+  .post(
+    "/bulk-delete",
+    clerkMiddleware(),
+    zValidator("json", z.object({ ids: z.array(z.string()) })),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const data = await db
+        .delete(accounts)
+        .where(
+          and(
+            eq(accounts.userId, auth.userId),
+            inArray(accounts.id, values.ids)
+          )
+        )
+        .returning({ id: accounts.id });
+
+      return c.json({ data });
+    },
+  )
 
 export default app;
